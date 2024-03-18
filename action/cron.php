51157a531472d6d2a6f7e37d218dcffecfc1ac35
Anna Dabrowska <dabrowska@cosmocode.de>
2024-03-18 19:47:58 +0100

    Move some functionality to a helper component

--- action/cron.php
+++ action/cron.php
@@ -44,96 +44,30 @@ class action_plugin_notification_cron extends DokuWiki_Action_Plugin
         $cron_helper = plugin_load('helper', 'notification_cron');
         $cron_helper->addUsersToCron();
 
-
         //get the oldest check
         $res = $sqlite->query('SELECT user, MIN(timestamp) FROM cron_check');
         $user = $sqlite->res2single($res);
-        //no user to sent notifications
+        //no user to send notifications to
         if (!$user) return;
 
         //update user last check
         $sqlite->query('UPDATE cron_check SET timestamp=? WHERE user=?',  date('c'), $user);
 
-        $plugins = [];
-        trigger_event('PLUGIN_NOTIFICATION_REGISTER_SOURCE', $plugins);
-        $notifications_data = [
-            'plugins' => $plugins,
-            'user' => $user,
-            'notifications' => []
-        ];
-        trigger_event('PLUGIN_NOTIFICATION_GATHER', $notifications_data);
+        // get new notifications from plugins
+        $notification_data = $cron_helper->getNotificationData($user);
 
-        $notifications = $notifications_data['notifications'];
-        //no notifications - nothing to sent
-        if (!$notifications) return;
+        //no notifications - nothing to send
+        if (empty($notification_data['notifications'])) return;
 
-        //get only notifications that has id
-        $notifications = array_filter($notifications, function ($notification) {
-            return array_key_exists('id', $notification);
-        });
-        //no notifications - nothing to sent
-        if (!$notifications) return;
+        $new_notifications = $cron_helper->getNewNotifications($user, $notification_data);
 
-        //get the notifications that has been sent already
-        $res = $sqlite->query('SELECT plugin, notification_id FROM notification WHERE user=?', $user);
-        $sent_notifications = $sqlite->res2arr($res);
-        $sent_notifications_by_plugin = [];
-        foreach ($plugins as $plugin) {
-            $sent_notifications_by_plugin[$plugin] = [];
-        }
-        foreach ($sent_notifications as $sent_notification) {
-            $plugin = $sent_notification['plugin'];
-            $id = $sent_notification['notification_id'];
-            $sent_notifications_by_plugin[$plugin][$id] = true;
-        }
-
-        $new_notifications = [];
-        foreach ($notifications as $notification) {
-            $plugin = $notification['plugin'];
-            $id = $notification['id'];
-            if (!isset($sent_notifications_by_plugin[$plugin][$id])) {
-                $new_notifications[] = $notification;
-            }
-        }
-
-        //no notifications - nothing to sent
+        // no notifications left - nothing to send
         if (!$new_notifications) return;
 
-        $html = '<p>' . $this->getLang('mail content');
-        $html .= '<ul>';
-        $text = $this->getLang('mail content') . "\n\n";
-
-        usort($new_notifications, function($a, $b) {
-            if ($a['timestamp'] == $b['timestamp']) {
-                return 0;
-            }
-            return ($a['timestamp'] > $b['timestamp']) ? -1 : 1;
-        });
-
-        foreach ($new_notifications as $notification) {
-            $content = $notification['full'];
-            $timestamp = $notification['timestamp'];
-
-            $date = strftime('%d.%m %H:%M', $timestamp);
-
-            $html .= "<li class=\"level1\"><div class=\"li\">$date $content</div></li>";
-            $text .= $date . ' ' . strip_tags($content). "\n";
-        }
-        $html .= '</ul></p>';
-
-        $mail = new Mailer();
-        $userinfo = $auth->getUserData($user, $requireGroups = false);
-        $mail->to($userinfo['name'].' <'.$userinfo['mail'].'>');
-        $mail->subject($this->getLang('mail subject'));
-        $mail->setBody($text, null, null, $html);
-        $mail->send();
-
-        //mark notifications as sent
-        foreach ($new_notifications as $notification) {
-            $plugin = $notification['plugin'];
-            $id = $notification['id'];
-            $sqlite->storeEntry('notification',
-                ['plugin' => $plugin, 'notification_id' => $id, 'user' => $user, 'sent' => date('c')]);
+        list($text, $html) = $cron_helper->composeEmail($new_notifications);
+        if ($cron_helper->sendMail($user, $text, $html)) {
+            //mark notifications as sent
+            $cron_helper->storeSentNotifications($user, $new_notifications);
         }
 
         $event->stopPropagation();
